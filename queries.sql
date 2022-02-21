27570d7515f63ed0d0e140f80caed472db7b527d
woodrat <levente.fodor@hotmail.com>
2022-02-21 16:36:40 +0100

    weighted risk prob

--- queries.sql
+++ queries.sql
@@ -76,21 +76,26 @@ where dd.date>='2021-03-31' and dd.date<='2021-05-31'
 group by dc.id ;
 
 -- ----------------------------------------- q#08 ----------------------------------
-select dd.date
-	, avg(exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4) / (1 + exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4))) as prob
+select date
+	, avg(exp(intercept + var1 + var2 + var3 + var4) / (1 + exp(intercept + var1 + var2 + var3 + var4))) as prob
+from (
+select distinct dd.date, dc.customer_type_name, dc.country_iso, ds.intercept, fs.*
 from dm.f_scoring fs
 inner join dm.d_scorecard ds on ds.id = fs.scorecard_id
 inner join dm.d_date dd on dd.id = fs.date_id 
 inner join dm.f_account fa on fa.customer_id = fs.customer_id and fa.date_id = fs.date_id
 inner join dm.d_account da on da.account_number = fa.account_number
 inner join dm.d_account_type dat on dat.id = da.account_type_id 
+inner join dm.d_customer dc on dc.id = fs.customer_id
 where dd.date>='2021-03-31' and dd.date<='2021-05-31'
-and dat.name not in('bankaccount','savingaccount')
-group by dd.date;
+and dat.name not in('bankaccount','savingaccount')) v
+group by date;
 
 -- ----------------------------------------- q#09 ----------------------------------
-select dc.customer_type_name
-	, avg(exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4) / (1 + exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4))) as prob
+select customer_type_name
+	, avg(exp(intercept + var1 + var2 + var3 + var4) / (1 + exp(intercept + var1 + var2 + var3 + var4))) as prob
+from (
+select distinct dd.date, dc.customer_type_name, dc.country_iso, ds.intercept, fs.*
 from dm.f_scoring fs
 inner join dm.d_scorecard ds on ds.id = fs.scorecard_id
 inner join dm.d_date dd on dd.id = fs.date_id 
@@ -99,12 +104,14 @@ inner join dm.d_account da on da.account_number = fa.account_number
 inner join dm.d_account_type dat on dat.id = da.account_type_id 
 inner join dm.d_customer dc on dc.id = fs.customer_id
 where dd.date='2021-04-30'
-and dat.name not in('bankaccount','savingaccount')
-group by dc.customer_type_name;
+and dat.name not in('bankaccount','savingaccount')) v
+group by customer_type_name;
 
 -- ----------------------------------------- q#10 ----------------------------------
-select dc.country_iso
-	, avg(exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4) / (1 + exp(ds.intercept + fs.var1 + fs.var2 + fs.var3 + fs.var4))) as prob
+select country_iso
+	, avg(exp(intercept + var1 + var2 + var3 + var4) / (1 + exp(intercept + var1 + var2 + var3 + var4))) as prob
+from (
+select distinct dd.date, dc.customer_type_name, dc.country_iso, ds.intercept, fs.*
 from dm.f_scoring fs
 inner join dm.d_scorecard ds on ds.id = fs.scorecard_id
 inner join dm.d_date dd on dd.id = fs.date_id 
@@ -113,7 +120,33 @@ inner join dm.d_account da on da.account_number = fa.account_number
 inner join dm.d_account_type dat on dat.id = da.account_type_id 
 inner join dm.d_customer dc on dc.id = fs.customer_id
 where dd.date='2021-04-30'
-and dat.name not in('bankaccount','savingaccount')
-group by dc.country_iso;
+and dat.name not in('bankaccount','savingaccount')) v
+group by country_iso;
+
+-- ----------------------------------------- q#11 ----------------------------------
+select date
+	, sum( balance * ( exp(intercept + var1 + var2 + var3 + var4) / (1 + exp(intercept + var1 + var2 + var3 + var4))) ) /
+		sum( balance ) as prob_wgt
+from (
+	select distinct dd.date, dc.customer_type_name, dc.country_iso, ds.intercept, fs.*
+	, (select sum(balance) 
+		from  dm.f_account fa2
+		where fa2.date_id = fa.date_id and fa2.customer_id = fa.customer_id
+		group by date_id, customer_id
+		) as balance
+	from dm.f_scoring fs
+	inner join dm.d_scorecard ds on ds.id = fs.scorecard_id
+	inner join dm.d_date dd on dd.id = fs.date_id 
+	inner join dm.f_account fa on fa.customer_id = fs.customer_id and fa.date_id = fs.date_id
+	inner join dm.d_account da on da.account_number = fa.account_number
+	inner join dm.d_account_type dat on dat.id = da.account_type_id 
+	inner join dm.d_customer dc on dc.id = fs.customer_id
+	where dd.date>='2021-03-31' and dd.date<='2021-05-31'
+	and dat.name not in('bankaccount','savingaccount') ) v
+group by date;
+
+-- ----------------------------------------- q#12 ----------------------------------
+
+-- ----------------------------------------- q#13 ----------------------------------
 
 -- =================================================================================
